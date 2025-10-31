38e3c3a56aceb3a4e3b035ab313c274973b9b06b
tomclima <tomecostalima1228@gmail.com>
2025-10-31 02:10:37 -0300

    overflow example

---
+++ overflow_simple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int variavellegalnaomudar = 13141592;
+    char username[10];
+
+    printf("escreva seu nome em menos de 10 caracteres, por favor: ");
+
+    scanf("%s", username);
+
+    printf("%d", variavellegalnaomudar);
+}
