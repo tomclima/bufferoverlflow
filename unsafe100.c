90dff2cdfaff445a41d328b4843a5e61b9ab9dcf
tomclima <tomecostalima1228@gmail.com>
2025-10-31 02:10:55 -0300

    remove reduntant

--- unsafe100.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int a[3] = {1 , 2 , 3};
-    a[100] = 1;
-    printf("%d \n", a[100]);
-}
--- unsafe1000.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int a[3] = {1 , 2 , 3};
-    a[1000] = 1;
-    printf("%d \n", a[1000]);
-}
